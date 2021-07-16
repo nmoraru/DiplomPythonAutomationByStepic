1b3565edbac175161ae862d946fa1ae2d1b11b43
bezoom <bezoom1990@gmail.com>
2021-07-16 20:17:48 +0300

    created login_page.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import time
+
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="en",
