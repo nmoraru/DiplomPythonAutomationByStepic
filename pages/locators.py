ed8642a58e27dd4fe23d0d51a6474eda7fe66293
bezoom <bezoom1990@gmail.com>
2021-07-18 17:11:47 +0300

    Add product_page to PageObject. Created tests for add product to basket.

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,10 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     LINK_TEXT = 'login'
+
+class ProductPageLocators():
+    BUTTON_FOR_ADD_PRODUCT_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_NAME_TO_SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div > .alert-success:nth-child(1) strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_PRICE_TO_SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div > .alert-info strong")
---
+++ pages/product_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+import re
+
+
+class ProductPage(BasePage):
+    def should_be_correct_success_message(self):
+        self.should_be_correct_product_name_to_success_message()
+        self.shoul_be_correct_product_price_to_success_message()
+        
+    def add_product_to_basket(self):
+        button_for_add_product_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_FOR_ADD_PRODUCT_TO_BASKET)
+        button_for_add_product_to_basket.click()
+
+    def should_be_correct_product_name_to_success_message(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_name_to_suceess_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_TO_SUCCESS_MESSAGE).text
+        assert re.search(product_name, product_name_to_suceess_message), "Incorrect product name to success message"
+
+    def shoul_be_correct_product_price_to_success_message(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        product_price_to_success_message = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_TO_SUCCESS_MESSAGE).text
+        assert product_price == product_price_to_success_message, "Incorrect product price to success message"
+        
