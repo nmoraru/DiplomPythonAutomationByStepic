9dd662746b4379560d16b3301a87189c6b1ed81f
nmoraru <59799725+nmoraru@users.noreply.github.com>
2021-07-19 19:41:46 +0300

    upd link to test product
    upd link to test product

--- test_product_page.py
+++ test_product_page.py
@@ -34,7 +34,7 @@ class TestUserAddToBasketFromProductPage():
 
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
@@ -42,7 +42,7 @@ def test_guest_can_add_product_to_basket(browser):
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()                       
@@ -74,7 +74,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
